c1134ff0eea1bf0ff6a7a7e6eb84cc847a21b4e2
danchikeyesgames <danchikeyesgames@gmail.com>
2021-03-19 23:53:32 +0300

    2 version

--- script.sh
+++ script.sh
@@ -2,36 +2,37 @@
 #LAB21
 
 file1=$1;
-count=$2
-i=0
+count=$2;
 if [ -e $file1 ]
     then 
         echo "The $file1 found"
         mkdir split
         cd split
         cp ../$file1 filetmp
-        for var in $(cat filetmp)
-            do
-                i=$(($i+1))
-            done
-        m=$(($i/$2))
-        if [ $(($i%$2)) -eq 0 ] 
-            then 
-                n=$m
-            else 
-                n=$(($m+1))
+        i=`wc -l < ../$1`
+        echo "strtok $i"
+        if [ $(($i%$count)) -eq 0 ]
+            then
+            n=$(($i/$count));
+            else
+            n=$(($i/$count));
+            n=$(($n+1));
         fi
+        echo $n
         for ((k=1;k<=n;k++)) {
-            #cat filetmp
             head -n$count filetmp > "output$k"
             echo "$k || $i"
-            i=$(($i-$2))
+            i=$(($i-$count))
             tail -n$i filetmp > tmp
             cat tmp > filetmp
         }
         rm tmp
         rm filetmp
         cd ..
+        if [ -e block ]
+            then
+            rm -rf block
+        fi
         mv split block
         echo "Finished..."
     else 
