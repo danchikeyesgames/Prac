d1c3da6ff3b09cb1131c472122861f1694c47de2
danchik <danchikhol@gmail.com>
2022-01-05 04:54:43 +0300

    add

---
+++ 1 sem/lab8/c.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1, num2 = 0, n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &num1);
+        if (num1 >= 10)
+            num2 = num2 + num1;
+    }
+    printf("%d\n", num2);
+
+    return 0;
+}
