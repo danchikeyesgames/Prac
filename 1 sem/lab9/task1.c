b44694fa52b1ecc9248eca5e63d42178c6c7a230
danchik <danchikhol@gmail.com>
2022-01-07 03:49:07 +0300

    lab 9

--- 1 sem/lab9/task1.c
+++ 1 sem/lab9/task1.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define N 50
 
 int main(void)
 {
     int i = 18, j = -9, l = 5;
+    int ik, jk, lk;
 
+    ik = i;
+    jk = j;
+    lk = l;
     for (int k = 1; k <= N; k++) {
-        i = 
+        i = mod(ik * max(jk, lk), 30) + mod(jk * min(ik, lk), 20) + k;
+        j = min(ik, max(jk, min(lk, max(ik - lk, jk - lk))));
+        l = sign(k - 10) * abs(ik - jk + lk - k);
+
+        
+
+        ik = i;
+        jk = j;
+        lk = l;
     }
 
     return 0;
