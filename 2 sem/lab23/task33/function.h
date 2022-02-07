571ff53ca261cff0c079f042f1ca4297979453da
danchik <danchikhol@gmail.com>
2022-02-07 21:44:05 +0300

    begin lab23

---
+++ 2 sem/lab23/task33/function.h
@@ -0,0 +1,19 @@
+#ifndef _FUNCTION_H_
+#define _FUNCTION_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct tree {
+    int key;
+    struct tree* left;
+    struct tree* right;
+} node;
+
+node*   CreateTree(int value);
+node*   InsertTree(node * root, int value);
+void    DeleteTree(node * root, int data);
+void    PrintTree(node * root);
+int     AmountVertex(node * root);
+
+#endif
