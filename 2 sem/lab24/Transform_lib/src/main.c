8590a977770e4c14574081202bee7808b14236d7
danchikeyesgames <danchikhol@gmail.com>
2022-08-09 00:56:40 +0300

    example usage

---
+++ 2 sem/lab24/Transform_lib/src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "../include/TransformToTree.h"
+
+int main(void) {
+    tr_vector* string = NULL;
+    tr_header tree;
+
+    tr_SetHeadNode(&tree);
+    string = tr_CollectNewVector();
+    // tr_PrintVector(string);
+    tr_CreateTransformedTree(string, string->length, &tree);
+    tr_OutputNormalFormat(&tree);
+    tr_PrintTreeFormat(&tree);
+
+    tr_ClearVector(string, 1);
+    tr_ClearTree(&tree);
+    return 0;
+}
